5c5fed1cff5d3e09cbf97326a8d38feffbd04a41
Sefukamil20R <Tenzilk20@gmail.com>
2024-06-25 06:26:30 +0300

    fix bug

--- Client/src/axios.jsx
+++ Client/src/axios.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const baseUrl = axios.create({
   baseURL: "http://localhost:4000/api",
-  // baseURL: "https://forumapi.samiyayusuf.com/api",
 });
 
 export default baseUrl;
